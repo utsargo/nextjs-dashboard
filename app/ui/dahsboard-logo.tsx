b042974bb90b51e0095c22f2856cd23e50ec0344
Utsargo Roy <royutsargo@gmail.com>
2024-07-03 21:48:00 +0600

    Implement CRUD

--- app/ui/dahsboard-logo.tsx
+++ app/ui/dahsboard-logo.tsx
@@ -4,9 +4,9 @@ import { lusitana } from '@/app/ui/fonts';
 export default function DashboardLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-col items-center justify-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row md:flex-col items-center justify-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <GlobeAltIcon className="h-12 w-12 md:h-24 md:w-24 rotate-[15deg]" />
       <p className="text-[44px]">NextDash</p>
     </div>
   );
